df8609ebfc0b9f26c22fb76ffa79fd964f7f5332
tregzmusic <tregzmusic@gmail.com>
2023-02-26 20:00:33 +0300

    ver. 0.1

--- AddMessage.py
+++ AddMessage.py
@@ -1,6 +1,6 @@
 import vk_api
-from config import all_token
 import time
+import configparser
 
 try:
     print('''.................................................__________......
@@ -15,13 +15,16 @@ try:
          .../  ____________ /_|           /  |__.\   \\ |             ||..
          __/_______________/.\___________/_____/..\___\\\____________//...\n''')
 
-    session = vk_api.VkApi(token=all_token)
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    session = vk_api.VkApi(token=config['VK']['token'])
     vk = session.get_api()
+    print('Получить токен - "https://vkhost.github.io/"')
+    print('Получить ID - "https://regvk.com/"')
     ID = str(input('Введите Ваш ID: '))
-    # Получить ID - https://regvk.com/
 
-    print('=================ПОЛУЧАЕМ СПИСОК ВАШИХ ДРУЗЕЙ==========================')
     print(f'========================ВАШ ID {ID}===============================')
+    print('=================ПОЛУЧАЕМ СПИСОК ВАШИХ ДРУЗЕЙ==========================')
 
 
     def get_friens(user_id):
@@ -37,21 +40,33 @@ try:
                 print(f'Status online: {user[0]["id"]}, {user[0]["first_name"]}, {user[0]["last_name"]}')
         print('')
 
-    def send_messages(send_id):
-        while True:
-            vk.messages.send(peer_id=int(input('Введите числовой id кому отправить сообщение: ')),
-                             message=str(input('Введите текст письма: ')), random_id=0)
-
-            s = '|'
-            for i in range(11):
-                time.sleep(0.05)
-                print('\r', 'Отправка', i * s, str(i) + str(0), '%', end='')
-            print(f'\nСообщение отправлено!︎')
-
-
 
     get_friens(ID)
-    send_messages(ID)
+
+    try:
+        def send_messages(send_id):
+            while True:
+                vk.messages.send(peer_id=int(input('Введите числовой id кому отправить сообщение: ')),
+                                 message=str(input('Введите текст письма: ')), random_id=0)
+
+                s = '|'
+                for i in range(11):
+                    time.sleep(0.05)
+                    print('\r', 'Отправка', i * s, str(i) + str(0), '%', end='')
+                print(f'\nСообщение отправлено!︎')
+
+
+        send_messages(ID)
+
+    except ValueError:
+        print('Синтаксическая ошибка!\nПроверьте правильность заполнения полей!')
+
+except KeyError:
+    print('Не указан токен в config.ini!')
+
+except vk_api.exceptions.ApiError:
+    print('Указан неверный ID или токен!')
+
 
 except KeyboardInterrupt:
-    print('Goodbye!')
+    print('\nGoodbye!')
---
+++ README.md
@@ -0,0 +1,13 @@
+<h1 align="center">Hi there, I'm Ilya</a> 
+<img src="https://github.com/blackcater/blackcater/raw/main/images/Hi.gif" height="32"/></h1>
+<h3 align="center">I am a Python programmer and musician from Russia</h3>
+
+# Данный скрипт позволяет узнать общее количество друзей в социальной сети VK, список онлайн друзей и отправлять сообщения по уникальному ID
+
+1) Для запуска необходима установить библиотеку:
+`pip install vk_api`
+2) Необходимо получить токен на портале `https://vkhost.github.io` и указать его в файле `config.ini`
+3) Получить Ваш ID или другого пользователя на портале `https://regvk.com/`
+
+
+    ver. 0.1 by tregzcode
